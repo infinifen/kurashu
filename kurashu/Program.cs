6fa27f25945ad7b9f14b71846f5d28f987d0c346
infinifen <48841404+infinifen@users.noreply.github.com>
2022-12-20 01:19:38 +0100

    Initial commit

---
+++ kurashu/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Diagnostics;
+using System.Media;
+using ThreadState = System.Diagnostics.ThreadState;
+
+// See https://aka.ms/new-console-template for more information
+
+List<int> trackedProcessIds = new();
+
+
+while (true)
+{
+    Thread.Sleep(1000);
+    GetHungProcesses();
+}
+
+void GetHungProcesses()
+{
+    var processes = Process.GetProcesses();
+    foreach (var p in processes)
+    {
+        p.Refresh();
+    }
+
+    var hung = processes.Where(p => !p.Responding &&
+                                    !p.Threads.OfType<ProcessThread>()
+                                        .Any(t => t.ThreadState == ThreadState.Wait &&
+                                                  t.WaitReason == ThreadWaitReason.Suspended));
+    foreach (var p in hung)
+    {
+        Console.WriteLine($"{p.ProcessName} ({p.Id}) {p.MainModule.FileName} is hung");
+        if (!trackedProcessIds.Contains(p.Id))
+        {
+            trackedProcessIds.Add(p.Id);
+            p.EnableRaisingEvents = true;
+            Console.WriteLine("print");
+            p.Exited += ((sender, eventArgs) =>
+            {
+                trackedProcessIds.Remove(p.Id);
+                Console.WriteLine("exited event handler");
+                var fs = File.Open(@"z.wav", FileMode.Open);
+                var sp = new SoundPlayer(fs);
+                sp.Play();
+                
+            });
+        }
+    }
+    foreach (var p in processes.Where(p => !trackedProcessIds.Contains(p.Id)))
+    {
+        p.Dispose();
+    }
+}
